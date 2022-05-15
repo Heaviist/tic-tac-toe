71e9a5d486efdfed1e70030002dc1302507f88f6
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-05-16 00:33:39 +0200

    Add check_result and related methods
    Tried to generalize most methods so it's easier to read and more concise without having to rewrite the same code multiple times. Following rubocop suggestions also helps to make smaller methods that handle a more overseeable amount of instructions.

--- hugs_and_kisses.rb
+++ hugs_and_kisses.rb
@@ -1,65 +1,77 @@
-# Start a game of Tic-Tac-Toe
-class TicTacToe
-  @@number_of_games = 0
+# frozen_string_literal: true
 
+WINNING_LINES = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
+
+# Start a game of Tic-Tac-Toe
+class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @@number_of_games += 1
+    @number_of_games = 1
     @game_board = *(1..9)
     @available_numbers = *(1..9)
-    @played_turns = 0
-    @turn = 1
   end
 
   def start
-    puts "Game #{@@number_of_games} will start now. The score is #{@player1.wins} - #{@player2.wins}."
+    puts "Game #{@number_of_games} will start now."
+    puts "The current score is #{@player1.wins} (#{@player1.name}) - #{@player2.wins} (#{@player2.name})."
     enter
-    # tutorial if @@number_of_games == 1
+    tutorial if @number_of_games == 1
     @game_board = Array.new(9, ' ')
-    @@number_of_games.odd? ? turn_player1 : turn_player2
+    @number_of_games.odd? ? turn(@player1) : turn(@player2)
   end
 
   private
 
-  def tutorial
-    puts 'You will only see this explanation for the first game you play. See the game grid below:'
+  def turn(player)
+    player_input(player)
     draw_game_board(@game_board)
-    enter
-    puts 'You will be asked to enter a number corresponding to the square you want to play your symbol in.'
-    puts 'After typing the number, press enter. You will see the move you have just played on the board.'
-    puts "If there\'s a winner, you can see the result and choose to play a new game.\n
-          The player that starts will alternate every game"
-    puts "\nReady?! Be careful out there! Your opponent is always around to give you a kiss or hug of death...\n\n"
-    enter
+    check_result(player) ? game_result(player) : turn(switch(player))
   end
 
-  def enter
-    puts 'Press "enter" to continue'
-    gets
-  end
-
-  def turn_player1
-    puts "#{@player1.name}, play your \"#{@player1.symbol}\", type and press enter:"
+  def player_input(player)
+    puts "#{player.name}, play your \"#{player.symbol}\"! Type the number of a free square and press enter:"
+    print 'Available squares '
+    @available_numbers.each { |n| print "- #{n} " }
+    print "\n"
     choice = gets.chomp.to_i
-    @available_numbers.include?(choice) ? @game_board[choice - 1] = @player1.symbol : turn_player1
+    @available_numbers.include?(choice) ? @game_board[choice - 1] = player.symbol : player_input(player)
     @available_numbers.delete(choice)
-    draw_game_board(@game_board)
-    turn_player2
   end
 
-  def turn_player2
-    puts "#{@player2.name}, play your \"#{@player2.symbol}\", type and press enter:"
-    choice = gets.chomp.to_i
-    @available_numbers.include?(choice) ? @game_board[choice - 1] = @player2.symbol : turn_player2
-    @available_numbers.delete(choice)
-    draw_game_board(@game_board)
-    turn_player1
+  def check_result(player)
+    result = WINNING_LINES.any? do |line|
+      line.all? { |index| @game_board[index] == player.symbol }
+    end
+    return result unless result == false && @available_numbers.empty?
+
+    puts "That\'s a draw!"
+    new_game?
   end
 
-  def check_result
+  def game_result(player)
+    puts "#{player.name} has won!"
+    player.wins += 1
+    new_game?
+  end
 
-    puts "no winner yet\n\n"
+  def new_game?
+    puts 'Do you want to play a new game (y/n)?'
+    if gets.chomp == 'y'
+      reset
+    else
+      puts "The final score is #{@player1.wins} (#{@player1.name}) - #{@player2.wins} (#{@player2.name})."
+    end
+  end
+
+  def reset
+    @number_of_games += 1
+    @available_numbers = *(1..9)
+    start
+  end
+
+  def switch(player)
+    player == @player1 ? @player2 : @player1
   end
 
   # takes an array with 9 numbers and draws the playing board
@@ -70,7 +82,23 @@ class TicTacToe
       print ((i + 1) % 3).zero? ? "\n---+---+---\n" : '|' unless (i + 1) == 9
     end
     print "\n\n"
-    check_result if @available_numbers.length < 7
+  end
+
+  def tutorial
+    puts 'You will only see this explanation for the first game you play. See the game grid below:'
+    draw_game_board(@game_board)
+    enter
+    puts 'You will be asked to enter a number corresponding to the square you want to play your symbol in.'
+    puts 'After typing the number, press enter. You will see the move you have just played on the board.'
+    puts "If there\'s a winner, you can see the result and choose to play a new game."
+    puts 'The player that starts will alternate every game'
+    puts "\nReady?! Be careful out there! Your opponent is always around to give you a kiss or hug of death...\n\n"
+    enter
+  end
+
+  def enter
+    puts 'Press "enter" to continue'
+    gets
   end
 end
 
@@ -97,5 +125,5 @@ player1.welcome
 puts 'Type the name of player 2 and press enter:'
 player2 = Player.new(gets.chomp, 'O')
 player2.welcome
-game = TicTacToe.new(player1, player2)
+game = Game.new(player1, player2)
 game.start
