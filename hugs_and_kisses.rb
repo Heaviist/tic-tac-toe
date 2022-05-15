f99658b5e2d94c10e8d2a91b4d5503e00455fbb9
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-05-15 20:29:36 +0200

    Add functions to play a turn for each player

--- hugs_and_kisses.rb
+++ hugs_and_kisses.rb
@@ -2,7 +2,9 @@
 class TicTacToe
   @@number_of_games = 0
 
-  def initialize
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
     @@number_of_games += 1
     @game_board = *(1..9)
     @available_numbers = *(1..9)
@@ -10,13 +12,12 @@ class TicTacToe
     @turn = 1
   end
 
-  def start(player1, player2)
-    puts "Game #{no_of_games} will start now. The score is #{player1.wins} - #{player2.wins}."
+  def start
+    puts "Game #{@@number_of_games} will start now. The score is #{@player1.wins} - #{@player2.wins}."
     enter
-    #tutorial if @@number_of_games == 1
-
+    # tutorial if @@number_of_games == 1
     @game_board = Array.new(9, ' ')
-    play_game(player1, player2)
+    @@number_of_games.odd? ? turn_player1 : turn_player2
   end
 
   private
@@ -27,6 +28,8 @@ class TicTacToe
     enter
     puts 'You will be asked to enter a number corresponding to the square you want to play your symbol in.'
     puts 'After typing the number, press enter. You will see the move you have just played on the board.'
+    puts "If there\'s a winner, you can see the result and choose to play a new game.\n
+          The player that starts will alternate every game"
     puts "\nReady?! Be careful out there! Your opponent is always around to give you a kiss or hug of death...\n\n"
     enter
   end
@@ -36,45 +39,28 @@ class TicTacToe
     gets
   end
 
-  def no_of_games
-    @@number_of_games
-  end
-
-  def play_game(player1, player2)
-    ## @@number_of_games.odd? ? play_turn_p1 : play_turn_p2
-    puts "#{player1.name}, play your #{player1.symbol}, type and press enter:"
+  def turn_player1
+    puts "#{@player1.name}, play your \"#{@player1.symbol}\", type and press enter:"
     choice = gets.chomp.to_i
-    @available_numbers.include?(choice) ? @game_board[choice - 1] = player1.symbol : play_game(player1, player2)
+    @available_numbers.include?(choice) ? @game_board[choice - 1] = @player1.symbol : turn_player1
     @available_numbers.delete(choice)
     draw_game_board(@game_board)
-    puts "#{player2.name}, play your #{player2.symbol}, type and press enter:"
+    turn_player2
+  end
+
+  def turn_player2
+    puts "#{@player2.name}, play your \"#{@player2.symbol}\", type and press enter:"
     choice = gets.chomp.to_i
-    @available_numbers.include?(choice) ? @game_board[choice - 1] = player2.symbol : play_game(player1, player2)
+    @available_numbers.include?(choice) ? @game_board[choice - 1] = @player2.symbol : turn_player2
     @available_numbers.delete(choice)
     draw_game_board(@game_board)
-    enter
-  end
-
-=begin
-  def play_turn_p1
-    puts "#{player1.name}: To place your \"#{player1.symbol}\", type your choice below and press enter."
-    choice = gets.chomp
-
-    check_result
-    play_turn_p2
-  end
-
-  def play_turn_p2
-    puts "#{player2.name}: To place your #{player2.symbol}, type your choice below and press enter."
-    check_result
-    play_turn_p1
+    turn_player1
   end
 
   def check_result
-    @played_turns += 1
-    @played_turns > 2 ? check_result : play_turn
+
+    puts "no winner yet\n\n"
   end
-=end
 
   # takes an array with 9 numbers and draws the playing board
   def draw_game_board(input)
@@ -84,24 +70,23 @@ class TicTacToe
       print ((i + 1) % 3).zero? ? "\n---+---+---\n" : '|' unless (i + 1) == 9
     end
     print "\n\n"
+    check_result if @available_numbers.length < 7
   end
 end
 
 # Add a new player to the game with a name
 class Player
-  @@players = 0
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
     @wins = 0
-    @@players += 1
   end
 
   attr_accessor :wins
   attr_reader :symbol, :name
 
   def welcome
-    puts "Welcome #{@name}! you are player #{@@players} and will play \"#{@symbol}\"s.\n\n"
+    puts "Welcome #{@name}! You will play \"#{@symbol}\"s.\n\n"
   end
 end
 
@@ -112,5 +97,5 @@ player1.welcome
 puts 'Type the name of player 2 and press enter:'
 player2 = Player.new(gets.chomp, 'O')
 player2.welcome
-game = TicTacToe.new
-game.start(player1, player2)
+game = TicTacToe.new(player1, player2)
+game.start
